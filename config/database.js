124fcb0451601651129ed47dce9ce80fdabdc363
maulanazulkifar <maulanazulkifar@gmail.com>
2022-11-09 21:20:07 +0700

    change database to free mysqlhosting

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 import {Sequelize} from "sequelize";
 
-const db = new Sequelize('porto_db', 'maul', '', {
-  host: '34.72.87.37',
+const db = new Sequelize('sql12555418', 'sql12555418', 'jYzCsRPmnL', {
+  host: 'sql12.freemysqlhosting.net',
   dialect: 'mysql'
 });
 
