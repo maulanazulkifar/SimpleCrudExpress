63d536d2076e4cbd94c7fdefcdbcf10b00afb4a4
maulanazulkifar <maulanazulkifar@gmail.com>
2022-11-09 20:28:13 +0700

    change db

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 import {Sequelize} from "sequelize";
 
-const db = new Sequelize('porto_db', 'root', '', {
-  host: 'localhost',
+const db = new Sequelize('porto_db', 'maul', '', {
+  host: '34.72.87.37',
   dialect: 'mysql'
 });
 
